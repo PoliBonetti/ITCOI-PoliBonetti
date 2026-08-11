ee21cf1b7d2b27911b053669f6b3efe4eee9893d
PoliBonetti <poliana.bonetti@aluno.senai.br>
2026-08-11 11:47:52 -0300

    Arduino LED Botao

---
+++ Introducao Arquitetura Iot/Aula 2/sketch_aug11a/sketch_aug11a.ino
@@ -0,0 +1,21 @@
+// C++ code
+//
+
+int led = 7;
+int botao = 2;
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  pinMode(botao, INPUT);
+}
+
+void loop()
+{
+  int valor = digitalRead(botao);
+  if (valor == HIGH){
+    digitalWrite(led, LOW);
+  } else {
+    digitalWrite(led, HIGH);
+  }
+}
